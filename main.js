9f1c8e52964feede5955366a4532114b3274765a
Ryder Dale <rydersdale@gmail.com>
2018-05-24 22:09:13 -0500

    all completed

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function categoryInitializationSelection(event) {
 
 let map;
 let infoWindow;
-let pos = {lat: -34.397, lng: 150.644};
+let pos = {lat: 30.265817399999996, lng: -97.7492271};
 document.getElementById('search').addEventListener('click', categoryInitializationSelection)
   
 function initMap() {
@@ -64,49 +64,56 @@ function initMap() {
 
     infoWindow = new google.maps.InfoWindow;
 
-    // Try HTML5 geolocation.
-    // if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(function(position) {
-    //         console.log("Fetching current position");
-    //         pos = {
-    //             lat: position.coords.latitude,
-    //             lng: position.coords.longitude
-    //         };
-            // console.log(pos);
-            infoWindow.setPosition(pos);
-            //tag for you are here
-            //infoWindow.open(map);
-            map.setCenter(pos);
-            var service = new google.maps.places.PlacesService(map);
-            service.nearbySearch({
-                location: pos,
-                radius: 500,
-                type: [categorySelection]
-              }, callback);
-            
-          
-            function callback(results, status) {
-              if (status === google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                  createMarker(results[i]);
-                }
-              }
-            }
-          
-            function createMarker(place) {
-              var placeLoc = place.geometry.location;
-              var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
-              });
+    infoWindow.setPosition(pos);
 
-              google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.setContent(place.name);
-                infoWindow.open(map, this);
-              });
+    map.setCenter(pos);
+    var service = new google.maps.places.PlacesService(map);
+    
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
             }
         }
-        console.log('Get Current Position');
+    };
+    
+    service.nearbySearch({
+        location: pos,
+        radius: 500,
+        type: [categorySelection]
+        }, callback);
+    
+    
+    
+    
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+        });
+
+     
+        let markerContent = '';
+
+
+    google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(markerContent);
+    // infoWindow.setContent(place.icon);
+    infoWindow.open(map, this);
+    });
+
+    if (place.rating) {
+        markerContent = '<div id="marker-content">'+'<h3>'+place.name+'</h3>'+'<h4> Rating: '+ place.rating +'</h4>'+
+'</div>';
+    }
+    else {
+        markerContent = '<div id="marker-content">'+'<h3>'+place.name+'</h3>'+'<h4> Rating: unavailable '+ '</h4>'+
+'</div>';
+    }
+    }
+}
+console.log('Get Current Position');
     
 
     
